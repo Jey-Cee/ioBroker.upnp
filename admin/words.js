4439683feea37870c6988dabeb50f02518168957
ldittmar <ldittmar.privat@gmail.com>
2019-02-22 12:49:44 +0100

    Add gulp auto translation

--- admin/words.js
+++ admin/words.js
@@ -2,48 +2,8 @@
 'use strict';
 
 systemDictionary = {
-    "upnp-discovery adapter settings":  {
-        "en": "Settings",
-        "de": "Einstellungen",
-        "ru": "настройки",
-        "pt": "Configurações",
-        "nl": "instellingen",
-        "fr": "Paramètres",
-        "it": "impostazioni",
-        "es": "Configuraciones",
-        "pl": "Ustawienia"
-    },
-    "enableSimplePlayerControls": {
-        "en": "Use simple media player objects",
-        "de": "Verwende einfache Media Player-Objekte",
-        "ru": "Используйте простые объекты медиаплеера",
-        "pt": "Use objetos simples do media player",
-        "nl": "Gebruik eenvoudige media player-objecten",
-        "fr": "Utiliser des objets de lecteur multimédia simples",
-        "it": "Usa oggetti di media player semplici",
-        "es": "Usa objetos simples de reproductor de medios",
-        "pl": "Używaj prostych obiektów odtwarzacza multimediów"
-    },
-    "autoDiscover": {
-        "en": "Auto-discover new devices",
-        "de": "Automatische Erkennung neuer Geräte",
-        "ru": "Автоматическое открытие новых устройств",
-        "pt": "Descobrir novos dispositivos automaticamente",
-        "nl": "Auto-ontdek nieuwe apparaten",
-        "fr": "Auto-découvrir de nouveaux périphériques",
-        "it": "Scoperta automaticamente nuovi dispositivi",
-        "es": "Detección automática de nuevos dispositivos",
-        "pl": "Automatyczne wykrywanie nowych urządzeń"
-    },
-    "autoSubscription": {
-        "en": "automatic subscription",
-        "de": "Automatisches Abonnement",
-        "ru": "автоматическая подписка",
-        "pt": "assinatura automática",
-        "nl": "automatisch abonnement",
-        "fr": "abonnement automatique",
-        "it": "abbonamento automatico",
-        "es": "suscripción automática",
-        "pl": "automatyczna subskrypcja"
-    }
+    "autoDiscover": {                                "en": "Auto-discover new devices",                       "de": "Automatische Erkennung neuer Geräte",             "ru": "Автоматическое открытие новых устройств",         "pt": "Descobrir novos dispositivos automaticamente",    "nl": "Auto-ontdek nieuwe apparaten",                    "fr": "Auto-découvrir de nouveaux périphériques",        "it": "Scoperta automaticamente nuovi dispositivi",      "es": "Detección automática de nuevos dispositivos",     "pl": "Automatyczne wykrywanie nowych urządzeń",         "zh-cn": "自动发现新设备"},
+    "autoSubscription": {                            "en": "automatic subscription",                          "de": "Automatisches Abonnement",                        "ru": "автоматическая подписка",                         "pt": "assinatura automática",                           "nl": "automatisch abonnement",                          "fr": "abonnement automatique",                          "it": "abbonamento automatico",                          "es": "suscripción automática",                          "pl": "automatyczna subskrypcja",                        "zh-cn": "自动订阅"},
+    "enableSimplePlayerControls": {                  "en": "Use simple media player objects",                 "de": "Verwende einfache Media Player-Objekte",          "ru": "Используйте простые объекты медиаплеера",         "pt": "Use objetos simples do media player",             "nl": "Gebruik eenvoudige media player-objecten",        "fr": "Utiliser des objets de lecteur multimédia simples", "it": "Usa oggetti di media player semplici",            "es": "Usa objetos simples de reproductor de medios",    "pl": "Używaj prostych obiektów odtwarzacza multimediów", "zh-cn": "使用简单的媒体播放器对象"},
+    "upnp-discovery adapter settings": {             "en": "Settings",                                        "de": "Einstellungen",                                   "ru": "настройки",                                       "pt": "Configurações",                                   "nl": "instellingen",                                    "fr": "Paramètres",                                      "it": "impostazioni",                                    "es": "Configuraciones",                                 "pl": "Ustawienia",                                      "zh-cn": "设置"},
 };
